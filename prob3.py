3d70d3273402274b8ed68559315ef283ab3a8dce
Austin Shin <shinaustin@gmail.com>
2015-12-24 02:36:06 -0800

    prob3ishard sigh

---
+++ prob3.py
@@ -0,0 +1,14 @@
+"""
+@author: Austin Shin
+https://projecteuler.net/problem=3
+Largest prime factor
+"""
+
+def primes_sieve(lim):
+    not_prime = []
+    for i in range(2, lim+1):
+        if i not in not_prime:
+            print(i)
+            for j in range(i*1, lim+1, i):
+                not_prime.append(j)
+print(primes_sieve(100))
