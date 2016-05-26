0b415406b8b2060854967589e3ad165f7182d3e6
Austin Shin <shinaustin@gmail.com>
2016-05-26 03:47:02 -0700

    prob8 solved

--- prob3.py
+++ prob3.py
@@ -6,9 +6,9 @@ Largest prime factor
 
 def primes_sieve(lim):
     not_prime = []
-    for i in range(2, lim+1):
+    for i in range(2, lim + 1):
         if i not in not_prime:
             print(i)
-            for j in range(i*1, lim+1, i):
+            for j in range(i * 1, lim + 1, i):
                 not_prime.append(j)
 print(primes_sieve(100))
---
+++ prob7.py
@@ -0,0 +1,12 @@
+"""
+@author: Austin Shin
+projeuler q7
+https://projecteuler.net/problem=7
+"""
+
+
+
+def prime_factorsUpTo(n):
+	lst = []
+	while (len(lst) != n):
+		
